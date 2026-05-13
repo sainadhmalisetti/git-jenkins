9cca2db93e1e630a59188a566f5e8f653b664582
Sainadh <sainadhmalisetti666@gmail.com>
2026-05-13 12:03:26 +0530

    this is the third commit for chnaging the build statemnets

--- demo.c
+++ demo.c
@@ -5,4 +5,5 @@ for(int i=0;i<10;i++){
 printf("%d",++a);
 }
 printf("this is printed after successful operation of git to jenkins manually ");
+printf("this is also for checking by chnaging build statements in the jenkins ");
 }
